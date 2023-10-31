330c84a305b695b637466ee293516f5a6593ef7f
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-31 15:13:22 +0100

    update public route order

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import './globals.css'
 import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
 
 const App = () => {
   return (
    <main className="flex h-screen">
       <Routes>
         {/* public routes */}
+        <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
         {/* private routes */}
         <Route index element={<Home />} />
       </Routes>
