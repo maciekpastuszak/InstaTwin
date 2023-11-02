9511aba1cbb7e011fe53fae38fa439070f0d48a3
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-02 08:52:23 +0100

    update right side bg img

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -13,7 +13,9 @@ const AuthLayout = () => {
         </section>
 
         <img 
-          src=""
+          src="/assets/images/side-img.svg"
+          alt="logo"
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
         />
         </>
       )}
