379496bce3aa43746bff41202f9d73fe0f9affd8
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-03 09:11:46 +0100

    update form definition

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,7 +15,10 @@ const SignupForm = () => {
     const form = useForm<z.infer<typeof SignupValidation>>({
       resolver: zodResolver(SignupValidation),
       defaultValues: {
+        name: "",
         username: "",
+        email: "",
+        password: ""
       },
     })
    
