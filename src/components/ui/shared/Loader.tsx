49d8512ea2dd6259198a766c66ceec6cb148e7db
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-05 22:21:16 +0100

    update loader component

--- src/components/ui/shared/Loader.tsx
+++ src/components/ui/shared/Loader.tsx
@@ -1,7 +1,9 @@
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex-center w-full">
+      <img src="/assets/icons/loader.svg" alt="loader" width={24} height={24} />
+    </div>
   )
 }
 
