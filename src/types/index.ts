041f8611165e1fec4469484a5bb9ae429a29b565
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-06 23:04:02 +0100

    add IUser and INewUser types

--- src/types/index.ts
+++ src/types/index.ts
@@ -30,3 +30,19 @@ export type INavLink = {
     location?: string;
     tags?: string;
   };
+
+  export type IUser = {
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+    imageUrl: string;
+    bio: string;
+  };
+  
+  export type INewUser = {
+    name: string;
+    email: string;
+    username: string;
+    password: string;
+  };
