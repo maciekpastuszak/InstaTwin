d81b4fc767d4ecd41cc14a9b674d3717144d5c7f
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-06 23:11:12 +0100

    update api.ts

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,10 @@
-export async function createUserAccount(user) {
-    
+import { INewUser } from "@/types";
+
+export async function createUserAccount(user: INewUser) {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
 }
