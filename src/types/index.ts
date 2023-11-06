2ad799c62b4ad782a2582cbf845aaef3f6c11316
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-06 23:02:43 +0100

    add INavLink and IUpdateUser types

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type INavLink = {
+    imgURL: string;
+    route: string;
+    label: string;
+  };
+  
+  export type IUpdateUser = {
+    userId: string;
+    name: string;
+    bio: string;
+    imageId: string;
+    imageUrl: URL | string;
+    file: File[];
+  };
