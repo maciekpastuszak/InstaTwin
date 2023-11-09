8a309ebb2791ec88aed9f2f91d4b30fdccbde066
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-09 09:31:20 +0100

    update useSignInAccount

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -15,6 +15,9 @@ export const useCreateUserAccount = () => {
 
 export const useSignInAccount = () => {
     return useMutation({
-        mutationFn: (user: INewUser) => createUserAccount(user)
+        mutationFn: (user: {
+            email: string;
+            password: string
+        }) => signInAccount(user)
     })
 }
