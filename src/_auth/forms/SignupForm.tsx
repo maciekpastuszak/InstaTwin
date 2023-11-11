e84cfcf86561022d48f5f1ef8a675d7b5e38d779
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-12 00:24:05 +0100

    add home navigation and failure logic

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
 import { SignupValidation } from "@/lib/validation"
 import Loader from "@/components/ui/shared/Loader"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useCreateUserAccount } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
 
@@ -18,6 +18,7 @@ import { useUserContext } from "@/context/AuthContext"
 const SignupForm = () => {
   const { toast } = useToast();
   const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
 
 const { mutateAsync: createUserAccount, isLoading: isCreatingUser } = useCreateUserAccount()
 
@@ -53,6 +54,14 @@ const { mutateAsync: signInAccount, isLoading: isSigningIn } = useSignInAccount(
      }
 
      const isLoggedIn = await checkAuthUser();
+
+     if(isLoggedIn) {
+      form.reset();
+
+      navigate('/')
+     } else {
+      toast({ title: 'Sign up failed. Please try again.'})
+     }
     }
 
   return (
