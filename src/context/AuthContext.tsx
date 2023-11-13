9e582506905a4ec7ea90d5e62a057ff5b6b95ec8
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-13 11:16:38 +0100

    bugfix-isPending

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -14,7 +14,7 @@ export const INITIAL_USER = {
 
 const INITIAL_STATE = {
     user: INITIAL_USER,
-    isLoading: false,
+    isPending: false,
     isAuthenticated: false,
     setUser: () => {},
     setIsAuthenticated: () => {},
@@ -23,7 +23,7 @@ const INITIAL_STATE = {
 
 type IContextType = {
   user: IUser;
-  isLoading: boolean;
+  isPending: boolean;
   setUser: React.Dispatch<React.SetStateAction<IUser>>;
   isAuthenticated: boolean;
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
@@ -34,7 +34,7 @@ const AuthContext = createContext<IContextType>(INITIAL_STATE)
 
 const AuthProvider = ( {children}: { children: React.ReactNode } ) => {
   const [user, setUser] = useState<IUser>(INITIAL_USER)
-  const [isLoading, setIsLoading] = useState(false);
+  const [isPending, setIsPending] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
   const navigate = useNavigate();
@@ -63,7 +63,7 @@ const AuthProvider = ( {children}: { children: React.ReactNode } ) => {
       console.log(error);
       return false;
     } finally {
-      setIsLoading(false);
+      setIsPending(false);
     }
   };
 
@@ -84,7 +84,7 @@ const AuthProvider = ( {children}: { children: React.ReactNode } ) => {
   const value = {
     user,
     setUser,
-    isLoading,
+    isPending,
     isAuthenticated,
     setIsAuthenticated,
     checkAuthUser
