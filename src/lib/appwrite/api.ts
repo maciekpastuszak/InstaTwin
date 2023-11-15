b78a2c0cc05d21b3688118b508312a27acdf2b11
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-15 09:55:15 +0100

    add signOutAccount api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -81,3 +81,13 @@ export async function GetCurrentUser() {
         console.log(error);
     }
 }
+
+export async function signOutAccount() {
+    try {
+        const session = await account.deleteSession("current");
+
+        return session;
+    } catch (error) {
+        console.log(error)
+    }
+}
