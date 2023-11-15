025e7162d431bce7fc976dd34705623b40aafb9d
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-15 09:46:21 +0100

    add logo img to topbar

--- src/components/ui/shared/Topbar.tsx
+++ src/components/ui/shared/Topbar.tsx
@@ -5,7 +5,12 @@ const Topbar = () => {
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
-          <img />
+          <img 
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
         </Link>
       </div>
     </section>
