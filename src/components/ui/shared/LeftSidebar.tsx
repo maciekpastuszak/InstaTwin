1a1caf506545b64426ad6b213868b394e037467a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-16 23:14:22 +0100

    add link with logo to left sidebar

--- src/components/ui/shared/LeftSidebar.tsx
+++ src/components/ui/shared/LeftSidebar.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
+
 const LeftSidebar = () => {
   return (
     <nav className="leftsidebar">
       <div className="flex flex-col gap-11">
-        <Link>
+        <Link to="/" className="flex gap-3 items-center">
+          <img 
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
         </Link>
       </div>
     </nav>
