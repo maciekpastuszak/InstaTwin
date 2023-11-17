679da180962d0b7d332d5bea5c1567501f7510a8
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-17 22:14:32 +0100

    style logo icon in leftSidebar

--- src/components/ui/shared/LeftSidebar.tsx
+++ src/components/ui/shared/LeftSidebar.tsx
@@ -28,7 +28,10 @@ const LeftSidebar = () => {
         <Link to={`/profile/${user.id}`}
         className="flex gap-3 items-center"
         >
-          <img src={user.imageUrl || "assets/icons/profile-placeholder.svg"} alt="" />
+          <img src={user.imageUrl || "assets/icons/profile-placeholder.svg"} 
+          alt="profile" 
+          className="h-14 w-14 rounded-full"
+          />
         </Link>
       </div>
     </nav>
