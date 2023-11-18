edb43aec17f91e9a2b564dd7e4b7cc56af8c38f4
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-18 23:41:18 +0100

    fix

--- src/components/ui/shared/LeftSidebar.tsx
+++ src/components/ui/shared/LeftSidebar.tsx
@@ -51,7 +51,7 @@ const LeftSidebar = () => {
             
             return (
               <li key={link.label} 
-              className={`leftsidebar-link ${isActive && 'bg-primary-500'}`}>
+              className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
                 <NavLink
                   to={link.route}
                   className="flex gap-4 items-center p-4"
