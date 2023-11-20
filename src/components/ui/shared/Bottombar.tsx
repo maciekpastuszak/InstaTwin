f2fc5e7ab1ff3150dbf87b02c5fafbe15536f49c
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-20 17:49:03 +0100

    add mapping through bottom links

--- src/components/ui/shared/Bottombar.tsx
+++ src/components/ui/shared/Bottombar.tsx
@@ -4,7 +4,26 @@ const Bottombar = () => {
   const { pathName } = useLocation();
   return (
    <section className="bottom-bar">
-
+      {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route;
+            
+            return (
+              <li key={link.label} 
+              className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
+                <NavLink
+                  to={link.route}
+                  className="flex gap-4 items-center p-4"
+                >
+                  <img 
+                    src={link.imgURL} 
+                    alt={link.label} 
+                    className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            )
+        )}
    </section>
   )
 }
