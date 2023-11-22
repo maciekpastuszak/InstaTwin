5cf13b9cf3b2e19de89f670cc9d2a1acac25b403
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-22 08:44:29 +0100

    add testing divs for the dropzone

--- src/components/ui/shared/FileUploader.tsx
+++ src/components/ui/shared/FileUploader.tsx
@@ -14,7 +14,11 @@ const FileUploader = () => {
      <div {...getRootProps()} className="fkex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer">
       <input {...getInputProps()} className="cursor-pointer " />
       {
-        isDragActive ?
+        fileUrl ? (
+          <div>test 1</div>
+        ) : (
+          <div>test 2</div>
+        )
           <p>Drop the files here ...</p> :
           <p>Drag 'n' drop some files here, or click to select files</p>
       }
