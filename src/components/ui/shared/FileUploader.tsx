7e62b09a3b6939846097206bcf1e039e4d295835
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-23 09:10:52 +0100

    bugfix FileUploader

--- src/components/ui/shared/FileUploader.tsx
+++ src/components/ui/shared/FileUploader.tsx
@@ -8,13 +8,14 @@ type FileUploaderProps {
 }
 
 const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
-  const [file, setFile] = useState([]);
+  const [file, setFile] = useState<File[]>([]);
   const [fileUrl, setFileUrl] = useState("");
 
-  const onDrop = useCallback((acceptedFiles: FileWithPath) => {
-    setFile(acceptedFiles);
-    fieldChange(acceptedFiles);
-    setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]));
   }, [file])
 
   const { getRootProps, getInputProps } = useDropzone({
