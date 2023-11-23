4c3cabb3311f17c66ed547ef534900b98da52ca3
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-23 08:55:02 +0100

    add paragraph and h33 to dropzone

--- src/components/ui/shared/FileUploader.tsx
+++ src/components/ui/shared/FileUploader.tsx
@@ -19,6 +19,8 @@ const FileUploader = () => {
         ) : (
           <div className="file_uploader-box">
             <img src="/assets/icons/file-upload.svg" width={96} height={77} alt="file-upload" />
+            <h3 className="base-medium text-light-2 mb-2 mt-6">Drag photo here</h3>
+            <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
           </div>
         )
       }
