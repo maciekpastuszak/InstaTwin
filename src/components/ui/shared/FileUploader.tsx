3c9916345db350e1bc8917f79994e0981738b7de
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-24 23:37:01 +0100

    update img container for droped files

--- src/components/ui/shared/FileUploader.tsx
+++ src/components/ui/shared/FileUploader.tsx
@@ -30,7 +30,13 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
       <input {...getInputProps()} className="cursor-pointer " />
       {
         fileUrl ? (
-          <div>test 1</div>
+          <div
+            className="flex flex-1 justify-center w-full p-5 lg:p-10"
+          >
+            <img src={fileUrl} 
+            alt="image" 
+            className="file_uploader-img" />
+          </div>
         ) : (
           <div className="file_uploader-box">
             <img src="/assets/icons/file-upload.svg" width={96} height={77} alt="file-upload" />
