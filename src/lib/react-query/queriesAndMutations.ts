1e3934c57fc44655f336e95e40a8004c7bb88ba3
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-27 09:31:01 +0100

    bugfix queriesandmutations

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -6,6 +6,7 @@ useQueryClient,
 useInfiniteQuery,
 } from '@tanstack/react-query'
 import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { QUERY_KEYS } from './queryKeys'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -33,9 +34,9 @@ export const useCreatePost = () => {
     return useMutation({
         mutationFn: (post: INewPost) => createPost(post),
         onSuccess: () => {
-            queryClient.invalidateQueries(
+            queryClient.invalidateQueries({
                 queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
-            )
+            })
         }
     })
 }
