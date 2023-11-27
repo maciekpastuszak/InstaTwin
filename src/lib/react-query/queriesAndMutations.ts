523a9f33e83ffb3393bf058c0c2ee1ad77031e23
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-27 08:42:00 +0100

    add useCreatePost mutation

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,11 +1,11 @@
-import { INewUser } from '@/types'
+import { INewPost, INewUser } from '@/types'
 import {
 useQuery,
 useMutation,
 useQueryClient,
 useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -25,5 +25,11 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
     return useMutation({
         mutationFn: signOutAccount
+    })
+}
+
+export const useCreatePost = () => {
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post)
     })
 }
