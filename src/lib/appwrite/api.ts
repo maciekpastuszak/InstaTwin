0fc5827407b0f7a701cbd3162d149c8b6b49f62a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-28 09:17:09 +0100

    update getRecentPosts

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -181,4 +181,8 @@ export async function getRecentPosts() {
         appwriteConfig.postCollectionId,
         [Query.orderDesc('$createdAt'), Query.limit(20)]
     )
+
+    if(!posts) throw Error;
+
+    return posts;
 }
