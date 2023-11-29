a75ee713559df4b95686338000f66d52bb0d53cb
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-29 09:32:27 +0100

    format createdAt const in PostCard

--- src/components/ui/shared/PostCard.tsx
+++ src/components/ui/shared/PostCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils"
 import { Models } from "appwrite"
 import { Link } from "react-router-dom"
 
@@ -23,7 +24,7 @@ const PostCard = ({ post }: PostCardProps) => {
                   </p>  
                 <div className="flex-center gap-2 text-light-3">
                   <p className="subtle-semibold lg:small-regular">
-                    {post.$createdAt}
+                    {formatDateString(post.$createdAt)}
                   </p>
                   -
                   <p className="subtle-semibold lg:small-regular">
