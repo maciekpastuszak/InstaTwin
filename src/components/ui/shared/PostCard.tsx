3bb1f6e252118a8954ad34968e2e6dd1bfb9b635
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-29 08:47:24 +0100

    update img in PostCard

--- src/components/ui/shared/PostCard.tsx
+++ src/components/ui/shared/PostCard.tsx
@@ -11,7 +11,10 @@ const PostCard = ({ post }: PostCardProps) => {
         <div className="flex-between">
             <div className="flex items-center gap-3">
               <Link to={`/profile/${post.creator.$id}`}>
-                <img src={post?.creator?.imageUrl || 'assets/icons/profile-placeholder.svg'} alt="" />
+                <img src={post?.creator?.imageUrl || 'assets/icons/profile-placeholder.svg'} 
+                  alt="creator" 
+                  className="rounded-full w-12 lg:h-12"
+                />
               </Link>
             </div>
         </div>
