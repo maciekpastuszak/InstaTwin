befbd1bbdd68843ade1e1ae7dba87c42022d24e8
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-30 09:04:25 +0100

    add check if user can edit the post

--- src/components/ui/shared/PostCard.tsx
+++ src/components/ui/shared/PostCard.tsx
@@ -1,3 +1,4 @@
+import { useUserContext } from "@/context/AuthContext"
 import { formatDateString } from "@/lib/utils"
 import { Models } from "appwrite"
 import { Link } from "react-router-dom"
@@ -7,6 +8,9 @@ type PostCardProps = {
 }
 
 const PostCard = ({ post }: PostCardProps) => {
+  const { user } = useUserContext();
+
+  if(!post.creator) return;
   return (
     <div className="post-card">
         <div className="flex-between">
@@ -34,7 +38,9 @@ const PostCard = ({ post }: PostCardProps) => {
               </div>
             </div>
 
-            <Link to={`/update-post/${post.$id}`}>
+            <Link to={`/update-post/${post.$id}`}
+              className={`${user.id !== post.creator.$id} && hidden`}
+            >
               <img src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
             </Link>
         </div>
