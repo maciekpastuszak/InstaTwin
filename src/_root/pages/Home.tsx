1750f42dfb18b9d27ad5eab5c21a1eab553e0b9b
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-11-30 09:12:13 +0100

    bugfix Home view

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
           ) : (
             <ul className="flexflex-col flex-1 gap-9 w-full">
               {posts?.documents.map((post: Models.Document) => (
-                <PostCard post={post}/>
+                <PostCard post={post} key={post.caption}/>
               ))}
             </ul>
           )}
