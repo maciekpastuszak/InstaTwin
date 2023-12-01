8fd10aede1937944c9f0c00f6e2b2a761ee71f3c
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-01 10:30:46 +0100

    add save icon to PostStats

--- src/components/ui/shared/PostStats.tsx
+++ src/components/ui/shared/PostStats.tsx
@@ -19,6 +19,17 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
             />
             <p className="small-medium lg:base-medium">0</p>
         </div>
+
+        <div className="flex gap-2">
+            <img 
+                src="/assets/icons/save.svg" 
+                alt="like" 
+                width={20} 
+                height={20}
+                onClick={() => {}}
+                className="cursor-pointer" 
+            />
+        </div>
     </div>
   )
 }
