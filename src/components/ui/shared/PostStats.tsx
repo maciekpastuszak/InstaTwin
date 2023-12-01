3a3e63ef29c4b0b71b201d871efa0e8d313c4c38
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-01 10:27:55 +0100

    add like icon to PsotStats

--- src/components/ui/shared/PostStats.tsx
+++ src/components/ui/shared/PostStats.tsx
@@ -9,7 +9,13 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   return (
     <div className="flex justify-between items-center z-20">
         <div className="flex gap-2 mr-5">
-
+            <img 
+                src="/assets/icons/liked.svg" 
+                alt="like" 
+                width={20} 
+                height={20}
+                onClick={() => {}}
+                className="cursor-pointer" />
         </div>
     </div>
   )
