5ebb4726ec47b3c28ef05d600bb074f1cb6c8fee
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-07 09:11:52 +0100

    update props in PostForm

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -8,7 +8,7 @@ const EditPost = () => {
   const { data: post, isPending } = useGetPostById(id || '');
 
   if(isPending) return <Loader />
-  
+
   return (
     <div className="flex flex-1">
       <div className="common-container">
@@ -21,7 +21,7 @@ const EditPost = () => {
           <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
         </div>
 
-        <PostForm />
+        <PostForm action="Update" post={post} />
       </div>
     </div>
   )
