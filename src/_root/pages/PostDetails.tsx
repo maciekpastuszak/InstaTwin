738ccc6e45d87e749f12b4c285a7ed120b4114ca
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-09 12:20:21 +0100

    add wrapper to PostDetails

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,11 +1,19 @@
+import Loader from '@/components/ui/shared/Loader';
 import { useGetPostById } from '@/lib/react-query/queriesAndMutations'
 import { useParams } from 'react-router-dom';
 
 const PostDetails = () => {
   const { id } = useParams()
   const { data: post, isPending } = useGetPostById(id || '');
+
   return (
-    <div>PostDetails</div>
+    <div className="post_details-container">
+      {isPending ? <Loader /> : (
+        <div className="post_details-card">
+          <img />
+        </div>
+      )}
+    </div>
   )
 }
 
