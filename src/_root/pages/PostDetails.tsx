3cd8336a92811d427e7ef951508886f506fac7ba
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-09 12:28:30 +0100

    update PostDetails styles

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -11,14 +11,13 @@ const PostDetails = () => {
     <div className="post_details-container">
       {isPending ? <Loader /> : (
         <div className="post_details-card">
-          <img src={post?.imageUrl} alt="post" className="post_details-img"/>
-          <div className="flex items-center gap-3">
-              <Link to={`/profile/${post?.creator.$id}`}>
+          <div className="flex-between w-full">
+          <Link to={`/profile/${post?.creator.$id}`}>
                 <img src={post?.creator?.imageUrl || 'assets/icons/profile-placeholder.svg'} 
                   alt="creator" 
                   className="rounded-full w-12 lg:h-12"
                 />
-              </Link>
+           
 
                 <div className="flex flex-col ">
                   <p className="base-medium lg:body-bold text-light-1">
@@ -34,6 +33,11 @@ const PostDetails = () => {
                   </p>
                 </div>
               </div>
+              </Link>
+          </div>
+          <img src={post?.imageUrl} alt="post" className="post_details-img"/>
+          <div className="post_details-info">
+
             </div>
         </div>
       )}
