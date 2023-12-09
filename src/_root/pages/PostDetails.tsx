1db317778ec87c60253856b264cd4e8a29f865e6
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-09 12:23:33 +0100

    update Links in PostDetails

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,6 +1,7 @@
 import Loader from '@/components/ui/shared/Loader';
 import { useGetPostById } from '@/lib/react-query/queriesAndMutations'
-import { useParams } from 'react-router-dom';
+import { formatDateString } from '@/lib/utils';
+import { Link, useParams } from 'react-router-dom';
 
 const PostDetails = () => {
   const { id } = useParams()
@@ -11,7 +12,7 @@ const PostDetails = () => {
       {isPending ? <Loader /> : (
         <div className="post_details-card">
           <div className="flex items-center gap-3">
-              <Link to={`/profile/${post.creator.$id}`}>
+              <Link to={`/profile/${post?.creator.$id}`}>
                 <img src={post?.creator?.imageUrl || 'assets/icons/profile-placeholder.svg'} 
                   alt="creator" 
                   className="rounded-full w-12 lg:h-12"
@@ -20,15 +21,15 @@ const PostDetails = () => {
 
                 <div className="flex flex-col ">
                   <p className="base-medium lg:body-bold text-light-1">
-                    {post.creator.name}
+                    {post?.creator.name}
                   </p>  
                 <div className="flex-center gap-2 text-light-3">
                   <p className="subtle-semibold lg:small-regular">
-                    {formatDateString(post.$createdAt)}
+                    {formatDate(post?.$createdAt)}
                   </p>
                   -
                   <p className="subtle-semibold lg:small-regular">
-                    {post.location}
+                    {post?.location}
                   </p>
                 </div>
               </div>
