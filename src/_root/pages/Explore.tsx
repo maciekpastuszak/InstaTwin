083da150be574c00e6e8ce5e32ff273d3a9ca5c7
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-11 10:00:19 +0100

    add title and search wrapper

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -4,7 +4,10 @@ const Explore = () => {
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
+        <h2 className="h3-bold md:h2-bold w-full">Search Post</h2>
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
 
+        </div>
       </div>
     </div>
   )
