66dd6d6bfc6fbf07d804669a171bd156035f4724
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-12 08:56:36 +0100

    typo

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -22,7 +22,7 @@ const Explore = () => {
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
         <h2 className="body-bold md:h3-bold">Popular Today</h2>
 
-        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-3 py-2 cusros-pointer">
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-3 py-2 cursor-pointer">
           <p className="small-medium md:base-medium text-light-2">All</p>
           <img src="/assets/icons/filter.svg" width={20} height={20} alt="filter" />
         </div>
