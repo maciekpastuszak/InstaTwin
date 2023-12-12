f075d17d63893e981e31f5169e379291b732bf2e
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-12 09:47:37 +0100

    typo

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -37,7 +37,7 @@ const Explore = () => {
           <SearchResuls />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
-        ) : post.pages.map((item, index) => (
+        ) : posts.pages.map((item, index) => (
           <GridPostList key={`page-${index}`} posts={item.documents} />
         ))}
       </div>
