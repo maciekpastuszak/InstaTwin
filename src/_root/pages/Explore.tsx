58d1b5a0bac6cdfabc00a291489a44e03a29faa2
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-12 08:56:20 +0100

    add filter icon

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -24,6 +24,7 @@ const Explore = () => {
 
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-3 py-2 cusros-pointer">
           <p className="small-medium md:base-medium text-light-2">All</p>
+          <img src="/assets/icons/filter.svg" width={20} height={20} alt="filter" />
         </div>
       </div>
     </div>
