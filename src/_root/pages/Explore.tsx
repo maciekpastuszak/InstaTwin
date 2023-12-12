bf424ab55dbf9a9d76afcad12626b45a96d4186a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-12 08:55:32 +0100

    add filter button to popualr posts

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -21,6 +21,10 @@ const Explore = () => {
       </div>
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
         <h2 className="body-bold md:h3-bold">Popular Today</h2>
+
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-3 py-2 cusros-pointer">
+          <p className="small-medium md:base-medium text-light-2">All</p>
+        </div>
       </div>
     </div>
   )
