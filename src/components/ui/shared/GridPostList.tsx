ff07a37c680750304b6c3bb807ad07f1a50d6e86
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-15 09:42:28 +0100

    add styles to post in GridPostList

--- src/components/ui/shared/GridPostList.tsx
+++ src/components/ui/shared/GridPostList.tsx
@@ -16,8 +16,8 @@ const GridPostList = ({ posts }: GridPostsListProps) => {
           key={post.$id}
           className="relative min-w-80 h-80"
         >
-          <Link to={`/posts/${post.$id}`}>
-            <img src={post.imageUrl} alt="" />
+          <Link to={`/posts/${post.$id}`} className="grid-post_link">
+            <img src={post.imageUrl} alt="post" className="h-full w-full object-cover" />
           </Link>
         </li>
       ))}
