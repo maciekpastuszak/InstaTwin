9fc8c8cb055e109a915dc87ab5f9409bec952445
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-18 23:22:49 +0100

    update styles and debug Explore

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -35,38 +35,54 @@ const Explore = () => {
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
-        <h2 className="h3-bold md:h2-bold w-full">Search Post</h2>
+        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
         <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
-          <img src="/assets/icons/search.svg" width={24} height={24} alt="search" />
-          <Input 
+          <img
+            src="/assets/icons/search.svg"
+            width={24}
+            height={24}
+            alt="search"
+          />
+          <Input
             type="text"
-            placeholder="search"
+            placeholder="Search"
             className="explore-search"
             value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
+            onChange={(e) => {
+              const { value } = e.target;
+              setSearchValue(value);
+            }}
           />
         </div>
       </div>
-      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
-        <h2 className="body-bold md:h3-bold">Popular Today</h2>
 
-        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-3 py-2 cursor-pointer">
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
           <p className="small-medium md:base-medium text-light-2">All</p>
-          <img src="/assets/icons/filter.svg" width={20} height={20} alt="filter" />
+          <img
+            src="/assets/icons/filter.svg"
+            width={20}
+            height={20}
+            alt="filter"
+          />
         </div>
       </div>
 
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
-          <SearchResults 
+          <SearchResults
             isSearchFetching={isSearchFetching}
             searchedPosts={searchedPosts}
           />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
-        ) : posts.pages.map((item, index) => (
-          <GridPostList key={`page-${index}`} posts={item.documents} />
-        ))}
+        ) : (
+          posts.pages.map((item, index) => (
+            <GridPostList key={`page-${index}`} posts={item.documents} />
+          ))
+        )}
       </div>
 
       {hasNextPage && !searchValue && (
@@ -75,7 +91,7 @@ const Explore = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default Explore
