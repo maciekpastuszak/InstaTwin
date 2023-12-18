4b5e10c18750d0aab04a39a607bcb7e47a69476b
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-12-18 23:20:38 +0100

    debug Createpost

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,14 +1,16 @@
-import PostForm from "@/components/ui/forms/PostForm"
+import PostForm from "@/components/forms/PostForm";
+
 
 const CreatePost = () => {
   return (
     <div className="flex flex-1">
       <div className="common-container">
         <div className="max-w-5xl flex-start gap-3 justify-start w-full">
-          <img src="/assets/icons/add-post.svg" 
-          alt="add"
-          width={36}
-          height={36} 
+          <img
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="add"
           />
           <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
         </div>
@@ -16,7 +18,7 @@ const CreatePost = () => {
         <PostForm action="Create" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
