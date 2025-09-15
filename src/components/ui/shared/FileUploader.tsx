b48c0063cd21458811b08bed4e80e1fae7dde668
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-15 11:11:10 +0200

    refactor: streamline component code and improve formatting across Profile, Saved, UpdateProfile, FileUploader, GridPostList, SearchResults, and API functions

--- src/components/ui/shared/FileUploader.tsx
+++ src/components/ui/shared/FileUploader.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useState } from 'react'
-import { FileWithPath, useDropzone } from 'react-dropzone'
-import { Button } from '../button';
+import { useCallback, useState } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+import { Button } from "../button";
 
 type FileUploaderProps = {
   fieldChange: (FILES: File[]) => void;
   mediaUrl: string;
-}
+};
 
 const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
   const [file, setFile] = useState<File[]>([]);
@@ -16,45 +16,48 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
       setFile(acceptedFiles);
       fieldChange(acceptedFiles);
       setFileUrl(URL.createObjectURL(acceptedFiles[0]));
-  }, [file])
+    },
+    [file]
+  );
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: {
-      'image/*' : ['.png', '.jpeg', '.jpg', '.svg']
-    }
-  })
+      "image/*": [".png", ".jpeg", ".jpg", ".svg"],
+    },
+  });
 
   return (
-     <div {...getRootProps()} className="fkex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer">
+    <div
+      {...getRootProps()}
+      className="fkex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
+    >
       <input {...getInputProps()} className="cursor-pointer " />
-      {
-        fileUrl ? (
-          <>
-            <div
-            className="flex flex-1 justify-center w-full p-5 lg:p-10"
-            >
-            <img src={fileUrl} 
-            alt="image" 
-            className="file_uploader-img" />
-            </div>
-            <p className="file_uploader-label">Click or drag photo to replace</p>
-          </>
-        ) : (
-          <div className="file_uploader-box">
-            <img src="/assets/icons/file-upload.svg" width={96} height={77} alt="file-upload" />
-            <h3 className="base-medium text-light-2 mb-2 mt-6">Drag photo here</h3>
-            <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
-          
-            <Button className="shad-button_dark_4">
-              Select from computer
-            </Button>
+      {fileUrl ? (
+        <>
+          <div className="flex flex-1 justify-center w-full p-5 lg:p-10">
+            <img src={fileUrl} alt="image" className="file_uploader-img" />
           </div>
-          
-        )
-      }
-    </div>
-  )
-}
+          <p className="file_uploader-label">Click or drag photo to replace</p>
+        </>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            width={96}
+            height={77}
+            alt="file-upload"
+          />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">
+            Drag photo here
+          </h3>
+          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
 
-export default FileUploader
+          <Button className="shad-button_dark_4">Select from computer</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,174 +1,191 @@
-import { INewPost, INewUser, IUpdatePost } from '@/types'
+import { INewPost, INewUser, IUpdatePost } from "@/types";
 import {
-useQuery,
-useMutation,
-useQueryClient,
-useInfiniteQuery,
-} from '@tanstack/react-query'
-import { GetCurrentUser, createPost, createUserAccount, deleteSavedPost, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts, signInAccount, signOutAccount, updatePost } from '../appwrite/api'
-import { QUERY_KEYS } from './queryKeys'
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import {
+  GetCurrentUser,
+  createPost,
+  createUserAccount,
+  deletePost,
+  deleteSavedPost,
+  getInfinitePosts,
+  getPostById,
+  getRecentPosts,
+  likePost,
+  savePost,
+  searchPosts,
+  signInAccount,
+  signOutAccount,
+  updatePost,
+} from "../appwrite/api";
+import { QUERY_KEYS } from "./queryKeys";
 
 export const useCreateUserAccount = () => {
-    return useMutation({
-        mutationFn: (user: INewUser) => createUserAccount(user)
-    })
-}
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
 
 export const useSignInAccount = () => {
-    return useMutation({
-        mutationFn: (user: {
-            email: string;
-            password: string
-        }) => signInAccount(user)
-    })
-}
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
 
 export const useSignOutAccount = () => {
-    return useMutation({
-        mutationFn: signOutAccount
-    })
-}
+  return useMutation({
+    mutationFn: signOutAccount,
+  });
+};
 
 export const useCreatePost = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: (post: INewPost) => createPost(post),
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
-            })
-        }
-    })
-}
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
 
 export const useGetRecentPosts = () => {
-    return useQuery({
-        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
-        queryFn: getRecentPosts,
-    })
-}
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+  });
+};
 
 export const useLikePost = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn: ({postId, likesArray }: {postId: string; likesArray: string[] }) =>
-            likePost(postId, likesArray), 
-            onSuccess: (data) => {
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id]
-                })
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
-                })
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_POSTS]
-                })
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_CURRENT_USER]
-                })
-            }
-    })
-}
+  return useMutation({
+    mutationFn: ({
+      postId,
+      likesArray,
+    }: {
+      postId: string;
+      likesArray: string[];
+    }) => likePost(postId, likesArray),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
+  });
+};
 
 export const useSavePost = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn: ({postId, userId }: {postId: string; userId: string }) =>
-            savePost(postId, userId), 
-            onSuccess: (data) => {
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
-                })
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_POSTS]
-                })
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_CURRENT_USER]
-                })
-            }
-    })
-}
+  return useMutation({
+    mutationFn: ({ postId, userId }: { postId: string; userId: string }) =>
+      savePost(postId, userId),
+    onSuccess: (_data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
+  });
+};
 
 export const useDeleteSavedPost = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn: (savedRecordId: string) =>
-            deleteSavedPost(savedRecordId), 
-            onSuccess: () => {
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
-                })
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_POSTS]
-                })
-                queryClient.invalidateQueries({
-                    queryKey: [QUERY_KEYS.GET_CURRENT_USER]
-                })
-            }
-    })
-}
+  return useMutation({
+    mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+      });
+    },
+  });
+};
 
 export const useGetCurrentUser = () => {
-    return useQuery({
-        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
-        queryFn: GetCurrentUser
-    })
-}
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+    queryFn: GetCurrentUser,
+  });
+};
 
 export const useGetPostById = (postId: string) => {
-    return useQuery({
-        queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
-        queryFn: () => getPostById(postId),
-        enabled: !!postId
-    })
-}
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_POST_BY_ID, postId],
+    queryFn: () => getPostById(postId),
+    enabled: !!postId,
+  });
+};
 
 export const useUpdatePost = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-      mutationFn: (post: IUpdatePost) => updatePost(post),
-      onSuccess: (data) => {
-        queryClient.invalidateQueries({
-          queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
-        });
-      },
-    });
-  };
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: IUpdatePost) => updatePost(post),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      });
+    },
+  });
+};
 
-  export const useDeletePost = () => {
-    const queryClient = useQueryClient();
-    
-    return useMutation({
-      mutationFn: ({ postId, imageId }: {postId: string, imageId: string}) => deletePost(postId, imageId),
-      onSuccess: () => {
-        queryClient.invalidateQueries({
-          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
-        });
-      },
-    });
-  };
+export const useDeletePost = () => {
+  const queryClient = useQueryClient();
 
-  export const useGetPosts = () => {
-    return useInfiniteQuery({
-        queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-        queryFn: getInfinitePosts,
-        getNextPageParam: (lastPage) => {
-            if(lastPage && lastPage.documents.length === 0) return null;
-        
-            const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
+  return useMutation({
+    mutationFn: ({ postId, imageId }: { postId: string; imageId: string }) =>
+      deletePost(postId, imageId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
 
-            return lastId;
-        }
-    });
-  };
+export const useGetPosts = () => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+    queryFn: getInfinitePosts,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      // Use page number for pagination
+      if (!lastPage || lastPage.documents.length === 0) return null;
+      return allPages.length; // next page number (0-based index)
+    },
+  });
+};
 
-  export const useSearchPosts = (searchTerm: string) => {
-    return useQuery({
-      queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
-      queryFn: () => searchPosts(searchTerm),
-      enabled: !!searchTerm,
-    });
-  };
+export const useSearchPosts = (searchTerm: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
+    queryFn: () => searchPosts(searchTerm),
+    enabled: !!searchTerm,
+  });
+};
